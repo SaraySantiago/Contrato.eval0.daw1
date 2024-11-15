ced0528be30b5ce5cf14bddaa72c2becf3f1f02c
SaraySantiago <ssantiago18@iesrioarba.es>
2024-11-15 13:13:55 +0100

    .

--- scaffolder.py
+++ scaffolder.py
@@ -2,40 +2,57 @@
 
 class monton():
  
-    __stuff = {} # bronce
+   # __stuff = {} # bronce
     # __stuff = () # bronce
-    # __stuff = [] # plata
+    stuff = [] # plata
     # __stuff = "" # PARA MASOCAS
     # __stuff = ... import maravilla # GLORIA
     
-    def __init__(self, *args: ...):
-        """doc..."""
-        pass
+    def __init__(self, *args):
+        """init regular y de copia"""
+        self.stuff = []
+        for item in args:
+            self.amontona(item)
 
     def __str__(self)-> str:
         """doc..."""
-        return "<<montonsito>>"
-    
+        answ = "<<"
+        for item in self.stuff:
+            answ += f"{str(item)}, "
+            answ += ">>"
+            return answ 
+                
     def amontona(self, algo):
-        """doc..."""
-        pass
+        """si algo no es monton, al monton. Si algo es monton, todo lo que tiene, al monton"""
+        if isinstance(algo, monton):
+            self.stuff.extend(algo.stuff)
+        else:
+            self.stuff.append(algo)
 
     def contiene(self, algo) -> bool:
-        """doc..."""
-        return True # pass
+        return algo in self.stuff
 
     def copiar(self, algo) -> object:
-        """doc..."""
-        return "Foo" # pass
+        if algo in self.stuff:
+            return algo
+        return None  #TODO: Raise ValueError
 
     def tomar(self, algo) -> object:
-        """doc..."""
-        return "Foo" # pass
+        if algo in self.stuff:
+            self.stuff.remove(algo)
+            return algo
+        return None #TODO: Raise ValueError
 
 if __name__ == "__main__":
     
     m0 = monton()        # m0 es un nuevo monton vacio
-    print(m0)
+    print("m0: ", m0)
+    
+    m2 = monton ("mas", "tomas")
+    print ("m2: ", m2)
+    
+    m3 = monton (11, "doce", 13, m2)
+    print ("m3: ", m3)
 
     m1 = monton("algo")  # m0 es un nuevo monton con "algo"
     m1.amontona("algo mas")
@@ -49,11 +66,7 @@ if __name__ == "__main__":
     m1.amontona("y mas mierdas")
     # assert: en m1 hay ...
 
-    # puedo amontonar -todo lo que haya- en un monton 
-    # en otro moton.
-    # Esto no vacia el monton argumentado (m1 aqui)
-    # Esto no vacia previamente el monton invocado (m0 aqui)
-    # no ~ m0 = m1
+    # puedo amontonar -todo lo que haya- en un monton en otro moton. Esto no vacia el monton argumentado (m1 aqui). Esto no vacia previamente el monton invocado (m0 aqui). no ~ m0 = m1
     m0.amontona(m1)
 
     predicado = m0.contiene("perla") # predicado == True
